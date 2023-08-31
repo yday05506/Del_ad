95ec0ddf077a24e92d66c75a7eb3d6a0016c9bed
yday05506 <yangdayeon2@gmail.com>
2023-08-31 09:43:18 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Del_ad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadA = new Thread(ThesMethod);
+            Thread threadB = new Thread(delegate ()
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("B");
+                }
+            });
+
+            Thread threadC = new Thread(delegate ()
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("C");
+                }
+            });
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+        }
+
+        private static void ThesMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
